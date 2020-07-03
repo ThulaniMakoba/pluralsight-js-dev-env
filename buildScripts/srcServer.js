d17ca0e72f0045a756e027b8bae30ac9cad152f0
tmakoba@bscglobal.com <tmakoba@bscglobal.com>
2020-07-03 21:53:49 +0200

    HTTP Calls with MOCK API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+  //Hard coding for simplicity. Pretend it a database
+  res.json([
+    {"id":1,"firstName":"Zeb", "lastName":"Mandela", "email":"xoxo.y.com"},
+    {"id":2,"firstName":"Zet", "lastName":"Jomo", "email":"joxo.y.com"},
+    {"id":3,"firstName":"Zen", "lastName":"Khoza", "email":"khxo.y.com"},
+    {"id":4,"firstName":"Ze", "lastName":"Jiba", "email":"jixo.y.com"}
+  ]);
+});
+
+
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
